02de26f88f8619208b0480289e252343415a7e6f
AV599625 <Avaratharajan6525@conestogac.on.ca>
2023-02-02 16:25:21 -0500

    Loops success

--- arnold.py
+++ arnold.py
@@ -8,37 +8,32 @@ prov = input("Enter your province: ").strip()
 pos = input("Enter your postal code: ").strip()
 spcInst = input("Enter any special delivery instructions: ")
 phNum = input("Enter your phone number: ").strip()
-dinChoice = str(input("Our daily specials are: \n\t1.Prime rib dinner \n\t2.Turkey Dinner"))
+dinChoice = str(input("Our daily specials are: \n\t1.Prime Rib Dinner - $25.50 \n\t2.Turkey Dinner - $29.50"))
 
 if dinChoice == '1' or dinChoice == '2'  :
         if dinChoice == '1' :
-            quanDin1 = float(input("How many of the Prime rib dinners would you like: "))
-             
+            quanDin1 = float(input("How many of the Prime rib dinners would you like: "))             
             while True:
-            
                 conf1 = str(input("Are you sure you would like to order: \n" + str(quanDin1) + "\tX Prime rib Dinners:" + "\n[Yes or No]: "))
-                if conf1 == 'y' or conf1 == 'Y' :
-                              
+                if conf1 == 'y' or conf1 == 'Y' :                              
                     sumT1 = (quanDin1 * din1)
                     print(str(sumT1))
-                    break
-                     
-            
+                    break                                 
                 if conf1 == 'n' or conf1 == 'N' :
                        break    
                 elif conf1 != 'y' or 'Y' or 'n' or 'N':
                        print("Invalid entry")         
-
-
         elif dinChoice == '2':
             quanDin2 = float(input("How many of the turkey dinners would you like: "))
             while True:
                 conf2 = input("Are you sure you would like to order: \n" + str(quanDin2) + "\tX  Turkey Dinners:" + "\n[Yes or No]: ")
                 if conf2 == 'y' or conf2 == 'Y' :
-                       break       
+                      sumT2 = (quanDin2 * din2)
+                      print(str(sumT2)) 
+                      break       
                 elif conf2 == 'n' or conf2 == 'N' :
                        break    
-                else:
+                elif conf2 != 'y' or 'Y' or 'n' or 'N':
                        print("Invalid entry")                
             else:
                    print("ok")
