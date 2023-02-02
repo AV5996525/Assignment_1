b3e69f84ab410bc56073b37fd9bcd6ad064f8492
AV599625 <Avaratharajan6525@conestogac.on.ca>
2023-02-02 11:50:40 -0500

    progression

--- arnold.py
+++ arnold.py
@@ -1,1 +1,12 @@
+print("Welcome to Arnold's Amazing Eats II \nHome of the best food in Waterloo! \nThis app will simply help you make an order!")
+firName = input("Enter your first name: ").strip()
+lasName = input("Enter your last name: ").strip()
+city = input("Enter your current city: ").strip()
+prov = input("Enter your province: ").strip()
+pos = input("Enter your postal code: ").strip()
+spcInst = input("Enter any special delivery instructions: ")
+phNum = input("Enter your phone number: ").strip()
+dinChoice = input("Our daily specials are: \n\t1.Prime rib dinner \n\t2.Turkey Dinner").strip()
+
+
 
