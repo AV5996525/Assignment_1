6909b1eb13fd259b126516fd01d0432fc0b6fa78
AV599625 <Avaratharajan6525@conestogac.on.ca>
2023-02-02 12:09:44 -0500

    menu

--- arnold.py
+++ arnold.py
@@ -8,5 +8,17 @@ spcInst = input("Enter any special delivery instructions: ")
 phNum = input("Enter your phone number: ").strip()
 dinChoice = input("Our daily specials are: \n\t1.Prime rib dinner \n\t2.Turkey Dinner").strip()
 
+if dinChoice.isdigit() == True :
+    if dinChoice == 1 :
+        print("How many of the Prime rib dinners would you like: ")
+    elif dinChoice == 2:
+        print("How many of the Turkey dinners would you like: ")
+    elif dinChoice != 1 or dinChoice != 2 :
+        print("Invalid choice, please select choice 1 or 2.")        
+if dinChoice.isalpha() :
+    print("Invalid response, please user numbers, not letters.")
+else:
+    pass
+
 
 
